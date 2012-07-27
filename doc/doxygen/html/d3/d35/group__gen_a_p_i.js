e804d70c8a4c4f818f7e21b51e60b1ee3be2e9ab
Radek Krejci <rkrejci@cesnet.cz>
2012-07-27 14:38:29 +0200

    doc: doxygen update

--- doc/doxygen/html/d3/d35/group__gen_a_p_i.js
+++ doc/doxygen/html/d3/d35/group__gen_a_p_i.js
@@ -6,6 +6,6 @@ var group__gen_a_p_i =
     [ "nc_err_new", "d3/d35/group__gen_a_p_i.html#ga057524ba5256c428b6bf88710f000e0c", null ],
     [ "nc_err_dup", "d3/d35/group__gen_a_p_i.html#ga316faef1140f1b48a2ed1de6d84d2bef", null ],
     [ "nc_err_free", "d3/d35/group__gen_a_p_i.html#gac4bd51febe24b517d1952d1c1d8f6b07", null ],
-    [ "nc_err_set", "d3/d35/group__gen_a_p_i.html#gaec254a2afeaaf75a274739bb90b620da", null ],
-    [ "nc_err_get", "d3/d35/group__gen_a_p_i.html#gaeffb585ffbe6142131c7851fc7c561b6", null ]
+    [ "nc_err_set", "d3/d35/group__gen_a_p_i.html#gab28d881eeebd79d485391ae1ffc97299", null ],
+    [ "nc_err_get", "d3/d35/group__gen_a_p_i.html#ga3d7714f1f56ac7203a80c61aea4344f8", null ]
 ];
--- doc/doxygen/html/db/d67/group__store.js
+++ doc/doxygen/html/db/d67/group__store.js
@@ -4,9 +4,10 @@ var group__store =
     [ "NC_DATASTORE", "db/d67/group__store.html#ga5e4b9ee138b963e8ea2354fef2ebbe73", null ],
     [ "NCDS_TYPE", "db/d67/group__store.html#gab0f0cbd5268577ec36fc76cf89c72a8f", null ],
     [ "ncds_new", "db/d67/group__store.html#ga35274d407f561c0358be08050e021179", null ],
-    [ "ncds_file_set_path", "db/d67/group__store.html#ga2a45352da9996851fe4d19ac1c68be1a", null ],
+    [ "ncds_file_set_path", "db/d67/group__store.html#gad48955dab497b1258d80019e542acb9b", null ],
     [ "ncds_init", "db/d67/group__store.html#gaabb1ae2c497726ad826fc6478f97e8ff", null ],
     [ "ncds_free", "db/d67/group__store.html#ga77b479cc1496b0c3883bb0f6bfe46754", null ],
     [ "ncds_free2", "db/d67/group__store.html#ga05e7f1ed8185b92a6dbf904172dba9d2", null ],
-    [ "ncds_apply_rpc", "db/d67/group__store.html#ga0dc4238eb2952da8241e401261174a70", null ]
+    [ "ncds_apply_rpc", "db/d67/group__store.html#gaabfd0bf6abeaf40659043bf082e14801", null ],
+    [ "ncds_break_locks", "db/d67/group__store.html#ga4158273f1ae0332638cc9343097c9f0a", null ]
 ];
