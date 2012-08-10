b7feb109c80036a0202206c615a095bad32c7f65
Kupka David <xkupka01@sauvignon.liberouter.org>
2012-08-10 13:43:37 +0200

    lib: BUGFIX: Assuming filter type to be subtree if not specified (according RCF6241).

--- src/messages.c
+++ src/messages.c
@@ -558,6 +558,10 @@ struct nc_filter * nc_rpc_get_filter (const nc_rpc * rpc)
 				if (xmlStrEqual(filter_node->name, BAD_CAST "filter")) {
 					retval = malloc (sizeof(struct nc_filter));
 					retval->type_string = (char*)xmlGetProp (filter_node, BAD_CAST "type");
+               /* implicit filter type is NC_FILTER_SUBTREE */
+               if (retval->type_string == NULL) {
+                  retval->type_string = strdup ("subtree");
+               }
 					if (xmlStrEqual(BAD_CAST retval->type_string, BAD_CAST "subtree")) {
 						retval->type = NC_FILTER_SUBTREE;
 						buf = xmlBufferCreate();
