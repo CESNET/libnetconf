46de4417fe48bbbef9dad7e9b7bb56b8245a7f90
Michal Vasko <vasko@liberouter.org>
2013-08-12 14:29:13 +0200

    transapi: CHANGE: diff is built as a tree
    Final commit for this change, fully working now.

--- src/transapi/transapi.c
+++ src/transapi/transapi.c
@@ -52,6 +52,57 @@ int transapi_xml_apply_callbacks_recursive(struct xmldiff_tree* tree, struct tra
 	return ret;
 }
 
+int transapi_apply_callbacks_recursive(struct xmldiff_tree* tree, struct transapi_xml_data_callbacks* calls, xmlDocPtr old_doc, xmlDocPtr new_doc) {
+	struct xmldiff_tree* child;
+	int min_prio, ret;
+
+	do {
+		min_prio = 0;
+		child = tree->children;
+		while (child != NULL) {
+			if (min_prio == 0 && !child->applied && child->priority > 0) {
+				/* Set minimal priority with a reasonable value (first child's priority) */
+				min_prio = child->priority;
+				child = child->next;
+				continue;
+			}
+
+			if (!child->applied && child->priority > 0 && child->priority < min_prio) {
+				min_prio = child->priority;
+			}
+			child = child->next;
+		}
+
+		if (min_prio > 0) {
+			child = tree->children;
+			while (child != NULL) {
+				if (!child->applied && child->priority == min_prio) {
+					/* Process this child recursively */
+					if (transapi_xml_apply_callbacks_recursive(child, calls) != EXIT_SUCCESS) {
+						return EXIT_FAILURE;
+					}
+					break;
+				}
+				child = child->next;
+			}
+		}
+	} while (min_prio > 0);
+
+	/* Finally call our callback */
+	DBG("Transapi calling callback %s with op %d.", tree->path, tree->op);
+	/* if node was removed, it was copied from old XML doc, else from new XML doc */
+	xmlNodeDump(buf, tree->op == XMLDIFF_REM ? old_doc : new_doc, tree->node, 1, 0);
+	node = (char*)xmlBufferContent(buf);
+	ret = calls->callbacks[tree->priority-1].func(tree->op, node, &calls->data);
+	xmlBufferEmpty(buf);
+	tree->applied = true;
+	if (ret != EXIT_SUCCESS) {
+		ERROR("Callback for path %s failed (%d).", tree->path, ret);
+	}
+
+	return ret;
+}
+
 /* will be called by library after change in running datastore */
 int transapi_xml_running_changed (struct transapi_xml_data_callbacks * c, const char * ns_mapping[], xmlDocPtr old_doc, xmlDocPtr new_doc, struct model_tree * model)
 {
@@ -92,28 +143,14 @@ int transapi_running_changed (struct transapi_data_callbacks * c, const char * n
 		xmldiff_free (diff);
 		return EXIT_FAILURE;
 	} else if (diff != NULL) {
-		//~ buf = xmlBufferCreate();
-		//~ for (i=0; i<diff->diff_count; i++) { /* for each diff*/
-			//~ DBG("(%d) %s\n", diff->diff_list[i].op, diff->diff_list[i].path);
-			//~ for (j=0; j<c->callbacks_count; j++) { /* find callback function */
-				//~ if (strcmp(diff->diff_list[i].path, c->callbacks[j].path) == 0) { /* exact match */
-					//~ /* call callback function */
-					//~ /* if node was removed, it was copied from old XML doc, else from new XML doc */
-					//~ xmlNodeDump(buf,diff->diff_list[i].op == XMLDIFF_REM ? old_doc : new_doc, diff->diff_list[i].node, 1, 0);
-					//~ node = (char*)xmlBufferContent(buf);
-					//~ if ((ret = c->callbacks[j].func(diff->diff_list[i].op, node, &c->data)) != EXIT_SUCCESS) {
-						//~ xmldiff_free (diff);
-						//~ return EXIT_FAILURE;
-					//~ }
-					//~ xmlBufferEmpty(buf);
-					//~ break;
-				//~ }
-			//~ }
-			//~ if (j == c->callbacks_count) { /* no callback function for given path found */
-				//~ VERB("Path %s(%d/%d) has no callback\n", diff->diff_list[i].path, i, diff->diff_count);
-			//~ }
-		//~ }
-		//~ xmlBufferFree(buf);
+		if (xmldiff_set_priorities(diff, c) != EXIT_SUCCESS) {
+			VERB("There was not found a single callback for the configuration change.");
+		} else {
+			if (transapi_apply_callbacks_recursive(diff, c, old_doc, new_doc) != EXIT_SUCCESS) {
+				xmldiff_free(diff);
+				return EXIT_FAILURE;
+			}
+		}
 	} else {
 		VERB("Nothing changed.\n");
 	}
