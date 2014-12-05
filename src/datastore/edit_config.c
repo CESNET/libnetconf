ad96474af36d6594075b8c6261c40d3411dbba18
Radek Krejci <rkrejci@cesnet.cz>
2014-12-05 17:18:28 +0100

    CHANGE: remove unnecessary error structure generation
    Missing element definition in the data model detected by is_leaf_list()
    is not a reason to generate NETCONF error. Validity of the configuration
    data is checked in other way (and can be omitted). So in this case just
    print warning but continue.

--- src/datastore/edit_config.c
+++ src/datastore/edit_config.c
@@ -2090,17 +2090,13 @@ static int edit_merge_lists(xmlNodePtr merged_node, xmlNodePtr edit_node, xmlDoc
 	return (0);
 }
 
-static int is_leaf_list(xmlNodePtr node, xmlDocPtr model, struct nc_err** error)
+static int is_leaf_list(xmlNodePtr node, xmlDocPtr model)
 {
 	xmlNodePtr model_node;
 
 	model_node = find_element_model(node, model);
 	if (model_node == NULL) {
 		WARN("unknown element %s!", (char* )(node->name));
-		if (error != NULL) {
-			*error = nc_err_new(NC_ERR_UNKNOWN_ELEM);
-			nc_err_set(*error, NC_ERR_PARAM_INFO_BADELEM, (char*) (node->name));
-		}
 		return (0);
 	} else if (xmlStrcmp(model_node->name, BAD_CAST "leaf-list") == 0) {
 		return (1);
@@ -2128,7 +2124,7 @@ static int edit_merge_recursively(xmlNodePtr orig_node, xmlNodePtr edit_node, xm
 			 * the value will be updated, in case of leaf-list, the item will
 			 * be created
 			 */
-			if (is_leaf_list(edit_node->parent, model, error)) {
+			if (is_leaf_list(edit_node->parent, model)) {
 				/*
 				 * according to RFC 6020, sec. 7.7.7, leaf-list entries can be
 				 * created or deleted, but they can not be modified
@@ -2212,9 +2208,7 @@ static int edit_merge_recursively(xmlNodePtr orig_node, xmlNodePtr edit_node, xm
 			}
 
 			/* find matching element to children */
-			if ((leaf_list = is_leaf_list(children, model, error)) == -1) {
-				return (EXIT_FAILURE);
-			}
+			leaf_list = is_leaf_list(children, model);
 			aux = orig_node->children;
 			while (aux != NULL && matching_elements(children, aux, keys, leaf_list) == 0) {
 				aux = aux->next;
@@ -2256,9 +2250,7 @@ static int edit_merge_recursively(xmlNodePtr orig_node, xmlNodePtr edit_node, xm
 				 * to move the node. In other cases (mainly leafs) we
 				 * don't care the content, because we want to change it.
 				 */
-				if ((leaf_list = is_leaf_list(children, model, error)) == -1) {
-					return (EXIT_FAILURE);
-				}
+				leaf_list = is_leaf_list(children, model);
 
 				while (aux != NULL) {
 					next = aux->next;
